ec0ad8a5b2ad1734a32ca160800b4ed7ce0ba8cb
Anthony Griffon <anthony@griffon.one>
2024-02-07 17:02:40 +0100

    feat: add instrumentation and watermak
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- app/wasmio/src/main.rs
+++ app/wasmio/src/main.rs
@@ -1,12 +1,36 @@
 mod infrastructure;
 use infrastructure::config::Cfg;
+use infrastructure::constant::VERSION;
+use infrastructure::instrumentation::Instruments;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = Cfg::from_env()?;
 
-    dbg!(config);
+    println!(
+        r###"
+ ▄     ▄ ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄▄▄▄▄▄ 
+█ █ ▄ █ █      █       █  █▄█  █   █       █
+█ ██ ██ █  ▄   █  ▄▄▄▄▄█       █   █   ▄   █
+█       █ █▄█  █ █▄▄▄▄▄█       █   █  █ █  █
+█       █      █▄▄▄▄▄  █       █   █  █▄█  █
+█   ▄   █  ▄   █▄▄▄▄▄█ █ ██▄██ █   █       █
+█▄▄█ █▄▄█▄█ █▄▄█▄▄▄▄▄▄▄█▄█   █▄█▄▄▄█▄▄▄▄▄▄▄█
 
-    println!("hello mom!");
+Version: {version}
+port: {port}
+addr: {addr}
+"###,
+        version = VERSION,
+        port = config.bind_addr.port(),
+        addr = config.bind_addr.ip(),
+    );
+
+    // Instrumentation
+    let _ = Instruments::new();
+
+    info!("Go");
+
     Ok(())
 }
