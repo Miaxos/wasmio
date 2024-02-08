2bd4309f8599933d76969be1ea8eca053dc43269
Anthony Griffon <anthony@griffon.one>
2024-02-08 15:50:11 +0100

    feat: add proper s3 answer
    Signed-off-by: Anthony Griffon <anthony@griffon.one>

--- app/wasmio/src/application/s3/errors.rs
+++ app/wasmio/src/application/s3/errors.rs
@@ -1,21 +1,94 @@
-use axum::http::StatusCode;
+use axum::{
+    body::Body,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use serde::{Deserialize, Serialize};
+use tracing::error;
 
 /// S3 partiel error code enum
 ///
 /// See [`ErrorResponses`](https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html)
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy, strum::Display)]
 #[allow(clippy::upper_case_acronyms)]
 #[non_exhaustive]
-pub enum S3ErrorCode {
+pub enum S3ErrorCodeKind {
     /// The specified bucket is not valid.
     InvalidBucketName,
 }
 
-impl S3ErrorCode {
+impl S3ErrorCodeKind {
     fn status_code(&self) -> StatusCode {
         match self {
-            S3ErrorCode::InvalidBucketName => StatusCode::BAD_REQUEST,
+            S3ErrorCodeKind::InvalidBucketName => StatusCode::BAD_REQUEST,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
+    }
+
+    fn message(&self) -> &'static str {
+        match self {
+            S3ErrorCodeKind::InvalidBucketName => "The specified bucket is not valid.",
+        }
+    }
+}
+
+pub struct S3HTTPError {
+    /// The bucket or object that is involved in the error.
+    ressource: String,
+    /// ID of the request associated with the error.
+    request_id: String,
+    kind: S3ErrorCodeKind,
+}
+
+impl S3HTTPError {
+    pub fn custom(ressource: &str, request_id: &str, kind: S3ErrorCodeKind) -> Self {
+        Self {
+            // TODO: little ugly but it's to have a proper impl quickly
+            request_id: request_id.to_string(),
+            ressource: ressource.to_string(),
+            kind,
+        }
+    }
+}
+
+// ------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize)]
+pub struct Error {
+    pub code: String,
+    pub message: &'static str,
+    pub ressource: String,
+    pub request_id: String,
+}
+
+impl IntoResponse for S3HTTPError {
+    fn into_response(self) -> axum::response::Response {
+        let err = match quick_xml::se::to_string(&Error {
+            code: self.kind.to_string(),
+            message: self.kind.message(),
+            ressource: self.ressource,
+            request_id: self.request_id,
+        }) {
+            Ok(elt) => elt,
+            Err(err) => {
+                error!("{err:?}");
+                return Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::new("wtf".to_string()))
+                    .unwrap();
+            }
+        };
+
+        let body = format!(
+            r###"<?xml version="1.0" encoding="UTF-8"?>
+{err}
+"###,
+            err = err
+        );
+
+        Response::builder()
+            .status(self.kind.status_code())
+            .body(Body::new(body))
+            .unwrap()
     }
 }
--- app/wasmio/src/application/s3/methods/bucket_create.rs
+++ app/wasmio/src/application/s3/methods/bucket_create.rs
@@ -1,26 +1,39 @@
 use axum::{
-    body::{Body, HttpBody},
+    body::Body,
     extract::{Host, State},
     http::{header, StatusCode},
-    response::{IntoResponse, Response},
-    Json,
+    response::Response,
 };
 use axum_serde::xml::Xml;
 use serde_aws_types::types::create_bucket_configuration::CreateBucketConfiguration;
 use tracing::info;
 
-use crate::application::s3::state::S3State;
+use crate::application::s3::{
+    errors::{S3ErrorCodeKind, S3HTTPError},
+    state::S3State,
+};
 
 pub async fn bucket_create_handle(
     Host(host): Host,
-    State(state): State<S3State>,
-    Xml(input): Xml<CreateBucketConfiguration>,
-) -> impl IntoResponse {
-    let bucket_name = host.split_once('.');
+    State(_state): State<S3State>,
+    Xml(_input): Xml<CreateBucketConfiguration>,
+) -> Result<Response, S3HTTPError> {
+    let bucket_name = match host.split_once('.') {
+        Some((bucket_name, _)) => bucket_name,
+        _ => {
+            return Err(S3HTTPError::custom(
+                "",
+                "",
+                S3ErrorCodeKind::InvalidBucketName,
+            ));
+        }
+    };
 
-    Response::builder()
+    info!(message = "Creating a new bucket", bucket = %bucket_name);
+
+    Ok(Response::builder()
         .status(StatusCode::OK)
-        .header(header::LOCATION, "wasmio")
+        .header(header::LOCATION, format!("/{bucket_name}"))
         .body(Body::empty())
-        .unwrap()
+        .unwrap())
 }
